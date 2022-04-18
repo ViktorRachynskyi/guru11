8b3aea1e5b4d87435fe1f6c464020ac8d8dca92c
Viktor Rachynskyi <viktor.rachynskyi@asquad.io>
2022-04-18 22:58:58 +0300

    add tess

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,74 @@
+
+
+package pages;
+
+import io.qameta.allure.Step;
+import models.User;
+import org.junit.jupiter.api.DisplayName;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationPage {
+
+    @Step("Open registration page")
+    public RegistrationPage openRegistrationPage() {
+        open("/automation-practice-form");
+        return this;
+    }
+
+    @Step("Fill registration form")
+    public RegistrationPage fillRegistrationForm(User user) {
+
+        // fields
+        $("#firstName").setValue(user.getFirstName());
+        $("#lastName").setValue(user.getLastName());
+        $("#userEmail").setValue(user.getEmail());
+        $("#userNumber").setValue(user.getPhone());
+
+        // radio buttons & checkboxes
+        $(byText(user.getSex())).click();
+        $(byText(user.getHobby())).click();
+
+        // calendar form
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption(user.getYear());
+        $(".react-datepicker__month-select").selectOption(user.getMonth());
+        $("[aria-label*='" + user.getDay() + "th']").click();
+
+        // dropdowns
+        $("#subjectsInput").setValue(user.getSubject()).pressEnter();
+        $("#currentAddress").setValue(user.getAddress());
+        $("#state").scrollTo().click();
+        $(byText(user.getState())).click();
+        $("#city").click();
+        $(byText(user.getCity())).click();
+
+        // uploading photo
+        $("#uploadPicture").uploadFromClasspath("pictures/" + user.getPhoto());
+        return this;
+    }
+
+    @Step("Click \"Submit\" button")
+    public RegistrationPage submitForm() {
+        $("#submit").click();
+        return this;
+    }
+
+    @Step("Verify that User created correct")
+    public void verifyThatUserCreated(User user) {
+        $(".table-responsive").shouldHave(
+                text("Student Name " + user.getFirstName() + " " + user.getLastName()),
+                text("Student Email " + user.getEmail()),
+                text("Gender " + user.getSex()),
+                text("Mobile " + user.getPhone()),
+                text("Date of Birth " + user.getDay() + " " + user.getMonth() + "," + user.getYear()),
+                text("Subjects " + user.getSubject()),
+                text("Hobbies " + user.getHobby()),
+                text("Picture " + user.getPhoto()),
+                text("Address " + user.getAddress()),
+                text("State and City " + user.getState() + " " + user.getCity()));
+    }
+}
---
+++ src/test/java/tests/RegistrationFormTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.*;
+import models.User;
+import pages.RegistrationPage;
+import tests.config.BaseTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Registration page Tests")
+public class RegistrationFormTest extends BaseTest {
+
+    @Test
+    void firstTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void registrationFormTest() {
+
+        User user = User.builder()
+                .firstName("Elon")
+                .lastName("Mask")
+                .email("tesla-mail@space.com")
+                .sex("Female")
+                .phone("0676767676")
+                .year("1998")
+                .month("January")
+                .day("17")
+                .subject("Physics")
+                .hobby("Music")
+                .photo("test.jpg") // use file name
+                .address("Mars str. 447/1")
+                .state("NCR")
+                .city("Delhi")
+                .build();
+
+        new RegistrationPage()
+                .openRegistrationPage()
+                .fillRegistrationForm(user)
+                .submitForm()
+                .verifyThatUserCreated(user);
+
+    }
+}
